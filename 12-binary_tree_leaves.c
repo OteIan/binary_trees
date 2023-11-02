4c5b91161601985924cb5289b6003b2c626e6da8
MarkMusili <musiliyrn@gmail.com>
2023-11-02 12:54:54 +0300

    How many nodes with atleast a child

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -15,7 +15,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL || tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
 	count_left = binary_tree_leaves(tree->left);
