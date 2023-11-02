873eaba88bcf76d19991212c453f7ae8d08e0026
MarkMusili <musiliyrn@gmail.com>
2023-11-02 14:56:04 +0300

    Compilation changes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,9 +16,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left)
-		height_left = 1 + binary_tree_balance(tree->left);
+		height_left = 1 + binary_tree_height(tree->left);
 	if (tree->right)
-		height_right = 1 + binary_tree_balance(tree->right);
+		height_right = 1 + binary_tree_height(tree->right);
 
 	balance = height_left - height_right;
 
