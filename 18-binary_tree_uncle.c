53a40233aa26c7556135711f9f1182b4707cb753
Ote Ian <oteian9@gmail.com>
2023-11-03 22:53:24 +0300

    Betty

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -20,9 +20,9 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 /**
  * binary_tree_uncle - Finds the uncle of a node
- * 
+ *
  * @node: Pointer to the node to find the uncle
- * 
+ *
  * Return: Pointer to the uncle node
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
