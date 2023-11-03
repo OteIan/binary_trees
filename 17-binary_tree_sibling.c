6d30658a876cce6d04cfff5ed1ed0cb6a926f4ee
Ote Ian <oteian9@gmail.com>
2023-11-03 22:45:36 +0300

    Betty

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_sibling - Finds the sibling of a node
- * 
+ *
  * @node: Pointer to the node to find the sibling
- * 
+ *
  * Return: pointer to the sibling node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
