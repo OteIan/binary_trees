227bd12b1f25d7ac1d19a50c615538bd19c03086
Ote Ian <oteian9@gmail.com>
2023-11-03 19:35:39 +0000

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -31,11 +31,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
  * @tree: Pointer to the root node of the tree to measure the size
  *
  * Return: If tree is NULL, return 0
-*/
+ */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t size_left, size_right;
-	
+
 	if (tree == NULL)
 		return (0);
 
